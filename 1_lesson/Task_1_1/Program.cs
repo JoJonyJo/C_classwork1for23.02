b2384cc3ebcaf6f2cf31507d6ed38e3d3dee5b65
JoJonyJo <funstreets@mail.ru>
2023-04-12 06:35:33 +1000

    task_1_0_ver_01 +

---
+++ 1_lesson/Task_1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿int s = int.Parse(Console.ReadLine()!);
+int x = int.Parse(Console.ReadLine()!);
+
+if (x == s/x)
+{
+     Console.WriteLine("Первое чилсо это квадрат второго");
+}
+else
+{
+    Console.WriteLine( "Первое чилсо не является квадратом второго");
+};    
