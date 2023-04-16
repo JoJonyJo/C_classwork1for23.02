9168de10cf68aa92430783e70af93d7aa6c5b5db
JoJonyJo <funstreets@mail.ru>
2023-04-16 16:47:05 +1000

    T_2_4 OK

---
+++ 2_lesson/T_2_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число");
+int N = int.Parse(Console.ReadLine()!);
+
+
+void Def(int N)
+{
+    if(N % 7 == 0 && N % 23 == 0) 
+        Console.WriteLine("Кратно");
+    else 
+        Console.WriteLine("Не кратно");
+}
+Def(N);
