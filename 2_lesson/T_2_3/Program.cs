aa27c7756b3c2e1a45dd2d691eb5a1c758cf072e
JoJonyJo <funstreets@mail.ru>
2023-04-16 16:24:21 +1000

    T_2_3 OK

---
+++ 2_lesson/T_2_3/Program.cs
@@ -0,0 +1,10 @@
+﻿void Def(int num1, int num2)
+{
+    if(num1 % num2 == 0)
+        Console.WriteLine("Кратно");
+    else 
+        Console.WriteLine("Не кратно, остаток " + num2 %num1);
+}
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+Def(a, b);
