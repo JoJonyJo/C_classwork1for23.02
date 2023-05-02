bfab614f579f541671be23c0086451fcda4ae838
JoJonyJo <funstreets@mail.ru>
2023-05-02 23:24:04 +1000

    T1 OK

---
+++ Lesson_3/L_3_0/Program.cs
@@ -0,0 +1,19 @@
+﻿// Принимает координаты точки (х!=0, у!=0). Выдает номер четверти, в которой находится эта точка.
+
+void Quarter(int x, int y)
+{
+    if (x==0 || y==0)
+        Console.WriteLine ("не соответствует условию");
+    if (x>0 && y>0)
+        Console.WriteLine (" I ");
+    if (x<0 && y>0)
+        Console.WriteLine (" II ");
+    if (x<0 && y<0)
+        Console.WriteLine (" III ");
+    if (x>0 && y<0)
+        Console.WriteLine (" IV ");
+
+}
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+Quarter(num_1, num_2);
