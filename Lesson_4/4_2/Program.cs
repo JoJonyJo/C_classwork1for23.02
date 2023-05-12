98049703b7711f9d3a4e31c306d7ff1fdf25d1ce
JoJonyJo <funstreets@mail.ru>
2023-05-12 22:30:51 +1000

    4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int Multiply (int num)
+{
+    int Multi = 1;
+    for (int i = 1; i <= num; i++)
+        Multi *= i;
+    return Multi;
+}
+int A = int.Parse(Console.ReadLine()!);
+int res = Multiply(A);
+Console.WriteLine(res);
