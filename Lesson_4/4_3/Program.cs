53a37f4f1c7c440e17f4c5131b4d649d19f93e86
JoJonyJo <funstreets@mail.ru>
2023-05-12 22:47:52 +1000

    4_3

---
+++ Lesson_4/4_3/Program.cs
@@ -0,0 +1,11 @@
+﻿void massive(int mas)
+{
+    int[] numbers = new int[mas];
+    for (int i=0; i < mas; i++)
+    {
+        numbers[i] = new Random().Next(0, 2);
+        Console.WriteLine(numbers[i]);
+    }
+}
+
+massive(8);
